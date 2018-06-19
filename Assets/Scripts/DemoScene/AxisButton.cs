e3680b1e372aebbbfca310cbabdbf5f4db8466a9
DaddyTrap <914670787@qq.com>
2018-06-19 17:45:12 +0800

    :fire: :sparkles: Delete some unused meta, add toggling axis

--- Assets/Scripts/DemoScene/AxisButton.cs
+++ Assets/Scripts/DemoScene/AxisButton.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class AxisButton : MonoBehaviour {
 
-    public void OnSelectAxisButtonClick()
-    {
+    public GameObject[] Axises;
 
+    bool hidden = false;
+
+    public void OnSelectAxisButtonClick() {
+        hidden = !hidden;
+        foreach (var axis in Axises) {
+            axis.SetActive(!hidden);
+        }
     }
 }
